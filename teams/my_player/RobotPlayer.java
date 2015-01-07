478a94bcab8ff2707cc4e14389bc51bac4d150a2
yandil <topiary@openmailbox.org>
2015-01-07 12:06:36 -0800

    learning to mine

--- teams/my_player/RobotPlayer.java
+++ teams/my_player/RobotPlayer.java
@@ -1,30 +1,47 @@
 package player1;
 import battlecode.common.*;
 import java.util.*;
+import java.util.Random; // this should give the same import 
 
 public class RobotPlayer {
 
+	static Direction facing;
+	static Random rand; 
+
 	public static void run(RobotController rc){
-		
+		rand = new Random(rc.getID()); 
+		facing = Direction.values()[(int)(rand.nextDouble()*8)]; // randomize starting direction 
 		while (true){
 			try {
-			// HQ can spawn units 
+				// HQ can spawn units 
 				if (rc.getType() == RobotType.HQ){
 					if (rc.isCoreReady() && rc.canSpawn(Direction.NORTH, RobotType.BEAVER)){
 						rc.spawn(Direction.NORTH, RobotType.BEAVER);
 					}
-				} else {
-					rc.move(Direction.NORTH);
+				} else if (rc.getType() == RobotType.BEAVER) { 
+
+					if (rc.senseOre(rc.getLocation()) > 1){
+						rc.mine(); // then mine, there is ore 
+					} else { 
+						if (rand.nextDouble() < 0.05){
+							if (rand.nextDouble() < 0.05) {
+								facing = facing.rotateLeft();
+							} else {
+								facing = facing.rotateRight();
+							}
+						}
+
+						if (rc.isCoreReady() && rc.canMove(facing)){
+							rc.move(facing);
+						}
+
+					}
 				}
+			
 			} catch (GameActionException e) {
-				if (rc.isCoreReady() && rc.canMove(Direction.North)){
-					
-				}
 				e.printStackTrace();
 			}
 			rc.yield();
 		}
-
-	}
-
+	} 
 }
