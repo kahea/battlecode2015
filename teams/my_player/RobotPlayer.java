cab319dd65bed83098016f0820ae2849780c55b0
yandil <topiary@openmailbox.org>
2015-01-07 15:04:45 -0800

    building miner factories

--- teams/my_player/RobotPlayer.java
+++ teams/my_player/RobotPlayer.java
@@ -4,48 +4,80 @@ import java.util.*;
 import java.util.Random; // this should give the same import 
 
 public class RobotPlayer {
-
 	static Direction facing;
 	static Random rand; 
 	static RobotController rc;
 
 	public static void run(RobotController myrc){
+		rc = myrc;
 		rand = new Random(myrc.getID()); // each robot will follow its own random path
-		facing = Direction.values()[(int)(rand.nextDouble()*8)]; // randomize starting direction 
+		facing = getRandomDirection();
 		while (true){
 			try {
 				// HQ can spawn units 
-				if (myrc.getType() == RobotType.HQ){
-					if (myrc.isCoreReady() && myrc.canSpawn(Direction.NORTH, RobotType.BEAVER)){
-						myrc.spawn(Direction.NORTH, RobotType.BEAVER);
-					}
-				} else if (myrc.getType() == RobotType.BEAVER) { 
-					// check if there is ore in current location 	
-					if (myrc.senseOre(myrc.getLocation()) > 1){
-						myrc.mine(); // if so, then mine it 
-					} else { 
-						moveAround();
-
-						if (myrc.isCoreReady() && myrc.canMove(facing)){
-							myrc.move(facing);
-						}
-
-					}
+				if (rc.getType() == RobotType.HQ){
+					spawnUnit(RobotType.BEAVER);
+				} else if (rc.getType() == RobotType.BEAVER) { 
+					buildStructure(RobotType.MINERFACTORY);
+					mineAndMove();
+				} else if (rc.getType() == RobotType.MINER){
+					mineAndMove();
+				} else if (rc.getType() == RobotType.MINERFACTORY){
+					spawnUnit(RobotType.MINER);
 				}
-			
 			} catch (GameActionException e) {
 				e.printStackTrace();
 			}
-			myrc.yield();
+			rc.yield();
 		}
 	}
-	private static void moveAround(){
+
+	private static void spawnUnit(RobotType type) throws GameActionException {
+		if (rc.isCoreReady() && rc.canSpawn(Direction.NORTH, type)){
+			rc.spawn(Direction.NORTH, type);
+		}
+	}
+
+	private static void moveAround() throws GameActionException {
 		if (rand.nextDouble() < 0.05){
 			if (rand.nextDouble() < 0.05) {
 				facing = facing.rotateLeft();
 			} else {
 				facing = facing.rotateRight();
-			}
+			} 
+		}
+		
+		if (rc.senseTerrainTile(rc.getLocation().add(facing)) != TerrainTile.NORMAL){
+			facing = facing.rotateLeft();
+		}
+		if (rc.isCoreReady() && rc.canMove(facing)){
+			rc.move(facing);
 		}
 	} 
+
+	private static void mineAndMove() throws GameActionException {
+		// check if there is ore in current location 	
+		if (rc.senseOre(rc.getLocation()) > 1){
+			if (rc.isCoreReady() && rc.canMine()) {
+				rc.mine(); // if so, then mine it 
+			}
+		} else { 
+			moveAround();
+		}
+	}
+
+	private static Direction getRandomDirection(){
+		// randomize direction 
+		return Direction.values()[(int)(rand.nextDouble()*8)];
+	}
+
+	private static void buildStructure(RobotType type) throws GameActionException {
+		if (rc.getTeamOre() > type.oreCost){
+			Direction build_dir = getRandomDirection();
+			if (rc.isCoreReady() && rc.canBuild(build_dir, type) ){
+				rc.build(build_dir, type);
+			}
+		}
+	}
+
 }
